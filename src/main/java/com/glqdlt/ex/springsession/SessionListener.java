b5cea548d80be74459c0ea059cdb86c89000bbce
jhun <jhun@nm-4ones.com>
2019-04-24 14:23:01 +0900

    sessionAttributeListener 추가

--- src/main/java/com/glqdlt/ex/springsession/SessionListener.java
+++ src/main/java/com/glqdlt/ex/springsession/SessionListener.java
@@ -2,16 +2,19 @@ package com.glqdlt.ex.springsession;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 /**
  * @author Jhun
  * @see <a href = 'https://stackoverflow.com/questions/36795678/how-to-enable-session-and-set-session-timeout-in-spring-security'>https://stackoverflow.com/questions/36795678/how-to-enable-session-and-set-session-timeout-in-spring-security</a>
+ * @see <a href = 'http://dveamer.github.io/backend/PreventDuplicatedLogin.html'>http://dveamer.github.io/backend/PreventDuplicatedLogin.html</a>
  * 2019-04-24
  */
 @Slf4j
-public class SessionListener implements HttpSessionListener {
+public class SessionListener implements HttpSessionListener, HttpSessionAttributeListener {
 
     @Override
     public void sessionCreated(HttpSessionEvent se) {
@@ -21,5 +24,15 @@ public class SessionListener implements HttpSessionListener {
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
         log.info("session destroyed ==> createtionTime : {}, session Id : {}", se.getSession().getCreationTime(), se.getSession().getId());
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent se) {
+
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent se) {
+
     }
 }
